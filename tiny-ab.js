64f100ffbce2fa903eddb81b01fba219939c7a1c
David Wiley <david.wiley@gmail.com>
2022-01-21 12:21:17 -0500

    Confirming changes

--- tiny-ab.js
+++ tiny-ab.js
@@ -3,18 +3,18 @@ const tinyABinit = () => {
     const contentAlt = document.getElementsByClassName('ab-test-alternative');
     const id = wp_user_info.username.data.display_name;
     const testCriteria = (contentAlt.length > 0) && (typeof id !== 'undefined');
-  
+    window.alert(id);
     testCriteria ? runTinyAB(contentOriginal, contentAlt, id) : removeElements(contentAlt);
   }
-  
+
   const removeElements = (group) => Array.from(group).forEach(element => element.remove());
-  
+
   const runTinyAB = (contentOriginal, contentAlt, id) => {
     const lastChar = id.slice(-1);
     const altChars = ['1', '3', '5', '7', '9', 'b', 'd', 'f'];
     const showAlt = altChars.includes(lastChar);
-  
+
     showAlt ? removeElements(contentOriginal) : removeElements(contentAlt);
   }
-  
+
   tinyABinit();
